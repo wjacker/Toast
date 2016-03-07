50418f95154604e0e62a05256fb0708e20f4db8c
Jack Wang <wjacker2@gmail.com>
2016-03-07 11:56:29 +0800

    add carthage support

---
+++ Toast/Toast.h
@@ -0,0 +1,20 @@
+//
+//  Toast.h
+//  Toast
+//
+//  Created by Jack Wang on 3/7/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import "UIView+Toast.h"
+
+//! Project version number for Toast.
+FOUNDATION_EXPORT double ToastVersionNumber;
+
+//! Project version string for Toast.
+FOUNDATION_EXPORT const unsigned char ToastVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Toast/PublicHeader.h>
+
+
